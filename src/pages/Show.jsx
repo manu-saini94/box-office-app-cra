b8115b6a628391bcc4a1306dea22c52d246b761d
manu-saini94 <manu.saini94@gmail.com>
2023-12-05 12:15:04 +0530

    displayed showdata in show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,7 +1,10 @@
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
-import { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
+import ShowMainData from '../components/shows/ShowMainData';
+import Details from '../components/shows/Details';
+import Seasons from '../components/shows/Seasons';
+import Cast from '../components/shows/Cast';
 
 // const useShowById = showId => {
 //   const [showData, setShowData] = useState(null);
@@ -36,7 +39,34 @@ const Show = () => {
     return <div>We have an error: {showError.message}</div>;
   }
   if (showData) {
-    return <div>Got show data: {showData.name}</div>;
+    return (
+      <div>
+        <ShowMainData
+          image={showData.image}
+          name={showData.name}
+          rating={showData.rating}
+          summary={showData.summary}
+          genres={showData.genres}
+        />
+        <div>
+          <h2>Details </h2>
+          <Details
+            status={showData.status}
+            premiered={showData.premiered}
+            network={showData.network}
+          />
+        </div>
+        <div>
+          <h2>Seasons</h2>
+          <Seasons seasons={showData._embedded.seasons} />
+        </div>
+
+        <div>
+          <h2>Cast</h2>
+          <Cast cast={showData._embedded.cast} />
+        </div>
+      </div>
+    );
   }
   return <div>Data is loading</div>;
 };
