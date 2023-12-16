9e78be27a160bde73c4570fbebf1ce203c506de9
manu-saini94 <manu.saini94@gmail.com>
2023-12-16 11:28:56 +0530

    deployed the app to github pages.added hashrouter in showcard

--- src/components/shows/ShowsCard.jsx
+++ src/components/shows/ShowsCard.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { SearchCard, SearchImgWrapper } from '../common/SearchCard';
 import { StarIcon } from '../common/StarIcon';
 import { useRef } from 'react';
+import { Link } from 'react-router-dom';
 
 const ShowsCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
   const summaryStripped = summary
@@ -29,9 +30,9 @@ const ShowsCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
       <h1>{name}</h1>
       <p>{summaryStripped}</p>
       <ActionSection>
-        <a href={`/show/${id}`} target="_blank" rel="noreferrer">
+        <Link to={`/show/${id}`} target="_blank" rel="noreferrer">
           Read More
-        </a>
+        </Link>
         <StarBtn
           ref={starBtnRef}
           type="button"
